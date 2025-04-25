f595f2af8f585631ae7f648b2e3c602c3f8f1ed3
Hadassa <166148227+Hadassa-gomes@users.noreply.github.com>
2025-04-24 22:34:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const app = express();
 
 const servicosRoutes = require('./routes/servicos');
 
-app.use(cors());
+// Configuração do CORS
+app.use(cors({
+  origin: 'uphonious-frangipane-8a69d7.netlify.app', // Substitua pelo endereço do seu front-end
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api/servicos', servicosRoutes);
@@ -17,3 +23,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
